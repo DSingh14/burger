8d66fa5df7b4f7027c4279a78c993f6a7f02d73e
Deepali Singh <14singh.deepali@gmail.com>
2019-07-22 21:46:34 -0500

    connection and handlebar

--- server.js
+++ server.js
@@ -21,11 +21,10 @@ app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgers_Controller.js");
 
-app.use(routes);
+app.use("/", routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function () {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
 });
-console.log('node process: ' + process.env.NODE_ENV)
