148eac27dcfc2bc6572398dd8c759ce86106d4de
Deepali Singh <14singh.deepali@gmail.com>
2019-07-26 15:01:48 -0500

    heroku fix

--- server.js
+++ server.js
@@ -4,31 +4,28 @@ var PORT = process.env.PORT || 3700;
 
 var app = express();
 
-
-
-
 // Parse application body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
+// // Set Handlebars.
+// var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
 
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/burgers_Controller.js");
+// // Import routes and give the server access to them.
+// var routes = require("./controllers/burgers_Controller.js");
 
-app.use("/", routes);
+// app.use("/", routes);
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + "/public"));
+// // Serve static content for the app from the "public" directory in the application directory.
+// app.use(express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
-    res.send("hellos")
-});
+    res.send("working");
+})
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function () {
