8b5826906f4c5638f5e22e744cffdbad62f7e64b
Deepali Singh <14singh.deepali@gmail.com>
2019-07-26 14:48:24 -0500

    add to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,44 @@
+
+// var dotenv = require(".dotenv")
+// dotenv.config();
+
 var mysql = require("mysql");
+var { root, Jaimatadi1, burgers_db } = process.env;
+
+
+// create connection
+var config = {
+    port: 3306,
+    host: 'localhost',
+    user: "root",
+    password: "Jaimatadi1",
+    database: "burgers_db"
+}
 var connection;
+var host;
 
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
+    host = 'JAWDB';
 } else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: "Jaimatadi1",
-        database: "burgers_db"
-    });
+    connection = mysql.createConnection(
+        config
+        // host: "localhost",
+        // port: 3306,
+        // user: "root",
+        // password: "Jaimatadi1",
+        // database: "burgers_db"
+    );
+    host = 'localhost';
 };
-connection.connect();
+connection.connect(function (err) {
+    if (err) {
+        console.log('error connecing: ' + err.stack);
+        return;
+    } else {
+        console.log('connected with' + host);
+    }
+});
 
 
 
